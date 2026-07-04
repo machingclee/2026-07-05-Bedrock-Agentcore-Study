ec3340e4ae59e4f7a9079b57a45700e1f0324a93
machingclee <machingclee@gmail.com>
2026-07-05 01:14:29 +0800

    Initial commit

---
+++ test/complete aws bedrock course.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CompleteAwsBedrockCourse from '../lib/complete aws bedrock course-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/complete aws bedrock course-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CompleteAwsBedrockCourse.CompleteAwsBedrockCourseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
